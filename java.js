9f03f7b03b97883826ba95d4253f0e8c32c94054
803933 <803933@sylvaniastudents.org>
2025-05-14 15:30:05 +0000

    i added my java from my other codespace

--- java.js
+++ java.js
@@ -1,21 +1,23 @@
-const canvas = document.querySelector("canvas")
-const c = canvas.getContext("2d")
 
-    let snake = [{ x: 5, y: 5, color: getRandomColor() }];
-    let food = { x: 10, y: 10 };
-    let direction = "right";
-
-    function drawSnake() {
-      snake.forEach((segment, index) => {
-        ctx.fillStyle = segment.color;
-        ctx.fillRect(segment.x * tileSize, segment.y * tileSize, tileSize, tileSize);
-
-        // Draw an eye on the head
-        if (index === 0) {
-          ctx.fillStyle = "black"; 
-          ctx.beginPath();
-          ctx.arc(segment.x * tileSize + tileSize / 2, segment.y * tileSize + tileSize / 4, 3, 0, Math.PI * 2);
-          ctx.fill();
-        }
-      });
+function drawSnakeBody() {
+    snakeBody.forEach((segment, index) => {
+        ctx.fillStyle = green;
+        ctx.fillRect(segment.x * 4, segment.y * 4, 4, 4); //figure out what this does and how to actually fill a snake in 
     }
+  )}
+
+  function updateGame(){
+    drawSnakeBody() //figure out the rest of the major functions that will be needed to start the game and then add them here. 
+}
+
+
+var gorillaBody
+
+functiondrawGorillaBody() {
+  gorillaBody.forEach((segment, index) => {
+    ctx.fillStyle = WebGLQuery;
+    ctx.fillRect(segment.x * 8, segment.y * 8)
+  }
+)}
+updateGame();
+
