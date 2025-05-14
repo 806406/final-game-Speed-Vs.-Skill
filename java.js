6d28f54ab835404b04d12aedac53e06745798b2c
Mason LaCasse <806406@sylvaniastudents.org>
2025-05-14 16:00:40 +0000

    made start button, cleaned up js

--- java.js
+++ java.js
@@ -1,23 +1,18 @@
-var snakeBody 
+var snakeBody
+var gorillaBody
 
 function drawSnakeBody() {
     snakeBody.forEach((segment, index) => {
         ctx.fillStyle = green;
         ctx.fillRect(segment.x * 4, segment.y * 4, 4, 4); //figure out what this does and how to actually fill a snake in 
     }
-  )}
-
-  function updateGame(){
-    drawSnakeBody() //figure out the rest of the major functions that will be needed to start the game and then add them here. 
+  )
 }
 
-
-var gorillaBody
-
-functiondrawGorillaBody() {
-    ctx.fillStyle = WebGLQuery;
-    ctx.fillRect(segment.x * 8, segment.y * 8)
-  }
+function drawGorillaBody() {
+  ctx.fillStyle = WebGLQuery;
+  ctx.fillRect(segment.x * 8, segment.y * 8)
+}
 updateGame();
 
 var gorillaAttack
@@ -48,4 +43,8 @@ var speedAttack
 
 function speedAttack(){
   
+}
+
+function updateGame() {
+  drawSnakeBody() //figure out the rest of the major functions that will be needed to start the game and then add them here. 
 }
